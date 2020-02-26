8df7f36cd095f0462540f53dad6940e898803904
Scott Bedard <scottbedard90@gmail.com>
2020-02-25 21:25:11 -0700

    add dummy data and test for calendar values

--- src/utils/heatmap.spec.js
+++ src/utils/heatmap.spec.js
@@ -35,5 +35,26 @@ describe('heatmap utils', () => {
             expect(stringifyDate(startDate)).toBe('2020-01-01');
             expect(stringifyDate(endDate)).toBe('2020-01-31');
         });
+
+        it('aggregates values', () => {
+            const calendar = getCalendar({
+                data: [
+                    { date: '2020-01-15T00:00:00', value: 1 },
+                    { date: '2020-01-15T00:00:00', value: 2 },
+                    { date: '2020-01-16T00:00:00', value: 5 },
+                ],
+                startDate: '2020-01-15T00:00:00',
+                endDate: '2020-01-16T00:00:00',
+                view: 'weekly',
+            });
+
+            expect(calendar[0].value).toBe(0);
+            expect(calendar[1].value).toBe(0);
+            expect(calendar[2].value).toBe(0);
+            expect(calendar[3].value).toBe(3);
+            expect(calendar[4].value).toBe(5);
+            expect(calendar[5].value).toBe(0);
+            expect(calendar[6].value).toBe(0);
+        });
     });
 });
